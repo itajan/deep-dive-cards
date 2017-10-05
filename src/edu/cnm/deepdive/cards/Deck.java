db267406889029dd026914a854258b4206340950
itajan <izaactajan@gmail.com>
2017-10-05 11:11:19 -0600

    Different types of classes, inlcuding lambdas.

--- src/edu/cnm/deepdive/cards/Deck.java
+++ src/edu/cnm/deepdive/cards/Deck.java
@@ -2,6 +2,7 @@ package edu.cnm.deepdive.cards;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 
@@ -35,6 +36,10 @@ public class Deck {
   public void sort() {
   //  sort(0, cards.size());
     Collections.sort(cards);
+  }
+
+  public void sort(Comparator<Card> comp) {
+    Collections.sort(cards, comp); // collections sort method
   }
 
   private void sort(int start, int end) {
--- src/edu/cnm/deepdive/cards/Test.java
+++ src/edu/cnm/deepdive/cards/Test.java
@@ -1,13 +1,84 @@
 package edu.cnm.deepdive.cards;
 
+import java.util.Comparator;
+
 public class Test {
 
   public static void main(String[] args) {
+    // This is now a local class below
+//    class AlternateSorter implements Comparator<Card> { // package private class. only one "public class" in a .java file
+//      // if you don't define a constructor, Java will define a default constructor
+//      @Override
+//      public int compare(Card card1, Card card2) {
+//        if (card1.rank != card2.rank) {
+//          return card1.rank.compareTo(card2.rank);
+//
+//        }
+//        return card1.suit.compareTo(card2.suit);
+//      }
+//
+//    }
+
+
     Deck deck = new Deck();
     deck.shuffle();
     System.out.println(deck);
     deck.sort();
     System.out.println(deck);
+
+    // Anonymous class
+//    deck.sort(new Comparator<Card>() {
+//      @Override
+//      public int compare(Card card1, Card card2) {
+//        if (card1.rank != card2.rank) {
+//          return card1.rank.compareTo(card2.rank);
+//        }
+//      return card1.suit.compareTo(card2.suit);
+//      }
+//    }); // to create instance use "new"
+
+    // Lambda expression, with explicit body and return
+//    deck.sort((card1, card2) -> {
+//        if (card1.rank != card2.rank) {
+//          return card1.rank.compareTo(card2.rank);
+//        }
+//        return card1.suit.compareTo(card2.suit);
+//    });
+//    System.out.println(deck);
+//
+//  }
+    // Lambda expression, with implicit body and return
+    deck.sort((card1, card2) -> (card1.rank != card2.rank)
+          ? card1.rank.compareTo(card2.rank)
+          : card1.suit.compareTo(card2.suit));
+    System.out.println(deck);
   }
 
+
+  // Nested class
+//  private static class AlternateSorter implements Comparator<Card> { // package private class. only one "public class" in a .java file
+//    // if you don't define a constructor, Java will define a default constructor
+//    @Override
+//    public int compare(Card card1, Card card2) {
+//      if (card1.rank != card2.rank) {
+//        return card1.rank.compareTo(card2.rank);
+//
+//      }
+//      return card1.suit.compareTo(card2.suit);
+//    }
+//
+//  }
+//
 }
+
+// Top-level (non-public) class
+//class AlternateSorter implements Comparator<Card> { // package private class. only one "public class" in a .java file
+//  // if you don't define a constructor, Java will define a default constructor
+//  @Override
+//  public int compare(Card card1, Card card2) {
+//    if (card1.rank != card2.rank) {
+//      return card1.rank.compareTo(card2.rank);
+//
+//    }
+//    return card1.suit.compareTo(card2.suit);
+//  }
